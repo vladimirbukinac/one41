3a16065dafa58241ab35c11b25ebc615812fdffc
Vladimir Bukinac <vladimir.bukinac@chess-ix.com>
2014-07-07 16:30:58 +0200

    FrontEnd Init...

---
+++ frontend/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name one41feApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the one41feApp
+ */
+angular.module('one41feApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
